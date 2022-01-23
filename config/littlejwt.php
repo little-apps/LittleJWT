cdad2eb6e4a2d2a0a17a2ce8c3cce0552184adc3
Nick H <nick@little-apps.com>
2022-01-23 16:26:05 -0700

    Updated config options

--- config/littlejwt.php
+++ config/littlejwt.php
@@ -1,5 +1,77 @@
 <?php
-// config for LittleApps/LittleJWT
-return [
 
+return [
+    'key' => [
+        /**
+         * The mechanism to sign/verify JWTs.
+         * Options: secret, file, or none.
+         */
+        'default' => 'secret',
+        'secret' => [
+            'phrase' => env('LITTLEJWT_KEY_PHRASE', '')
+        ],
+        'file' => [
+            /**
+             * The type of file.
+             * Options: pem, p12, or crt.
+             */
+            'type' => 'pem',
+            'path' => '/path/to/my/key/file.pem',
+            /**
+             * The secret to use if the file is encrypted.
+             * This does not apply to crt files.
+             * Leave empty if file is not encrypted.
+             */
+            'secret' => ''
+        ],
+    ],
+    /**
+     * The algorithm used by Little JWT.
+     */
+    'algorithm' => \Jose\Component\Signature\Algorithm\HS256::class,
+    'blacklist' => [
+        /**
+         * Blacklist driver to use for storing blacklisted JWTs.
+         */
+        'driver' => 'cache',
+        'cache' => [
+            /**
+             * How long a JWT stays in the blacklist (in seconds).
+             * Set to 0 to have JWTs blacklisted forever.
+             */
+            'ttl' => 0
+        ],
+        'database' => [
+            'table' => 'jwt_blacklist',
+            'columns' => [
+                /**
+                 * The name of the column that holds the JWT identifier.
+                 */
+                'identifier' => 'jwt',
+                /**
+                 * The name of the column that holds the expiry date/time for the JWT.
+                 */
+                'expiry' => 'expires_at'
+            ]
+        ]
+
+    ],
+    /**
+     * Claim options to use for the default generated JWTs.
+     */
+    'claims' => [
+        'alg' => 'HS256',
+        'ttl' => 3600, // Number of seconds before JWT expiry.
+        'leeway' => 0,
+        'iss' => env('APP_URL', 'http://localhost'),
+        'aud' => env('APP_NAME', 'Laravel'),
+        'required' => [
+            'header' => ['alg'],
+            'payload' => ['iss', 'iat', 'exp', 'nbf']
+        ],
+        /**
+         * Claims that are timestamps and need to be serialized.
+         */
+        'timestamps' => ['iat', 'nbf', 'exp']
+    ]
 ];
