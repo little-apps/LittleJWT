252832ebf002d42fb00dd5deffe69a112d86c51d
Nick H <nick@little-apps.com>
2022-01-30 21:05:40 -0700

    validJWT method generates a TestValid instance, which acts a proxy for the Valid instance

---
+++ src/Testing/TestValid.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LittleApps\LittleJWT\Testing;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Traits\ForwardsCalls;
+
+use Jose\Component\Core\JWK;
+
+use LittleApps\LittleJWT\JWT\JWT;
+use LittleApps\LittleJWT\Validation\Valid;
+
+class TestValid {
+    use ForwardsCalls;
+
+    protected $app;
+    protected $valid;
+
+    public function __construct(Application $app, Valid $valid)
+    {
+        $this->app = $app;
+        $this->valid = $valid;
+    }
+
+    /**
+     * Builds a Validator
+     *
+     * @return Validator
+     */
+    protected function buildValidator()
+    {
+        $validator = $this->valid->buildValidator();
+
+        return new TestValidator($this->app, $validator);
+    }
+
+    /**
+     * Handle dynamic calls into macros or pass missing methods to the base valid instance.
+     *
+     * @param  string  $method
+     * @param  array  $args
+     * @return mixed
+     */
+    public function __call($method, $args)
+    {
+        return $this->forwardCallTo($this->valid, $method, $args);
+    }
+}
