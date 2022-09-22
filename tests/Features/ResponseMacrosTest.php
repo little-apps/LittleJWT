e0b546d0e8bdc3a782ab99eca9519ec466d6c265
Nick H <nick@little-apps.com>
2022-09-22 01:12:03 -0600

    Added Response macros tests

---
+++ tests/Features/ResponseMacrosTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LittleApps\LittleJWT\Tests\Features;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+
+use LittleApps\LittleJWT\Guards\Adapters\GenericAdapter;
+use LittleApps\LittleJWT\Tests\Concerns\CreatesUser;
+use LittleApps\LittleJWT\Tests\Concerns\InteractsWithLittleJWT;
+use LittleApps\LittleJWT\Tests\TestCase;
+
+class ResponseMacrosTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+    use CreatesUser;
+    use InteractsWithLittleJWT;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Auth::setAdapter($this->app[GenericAdapter::class]);
+    }
+
+    /**
+     * Tests that a JWT is included in response for login.
+     *
+     * @return void
+     */
+    public function test_valid_login_json()
+    {
+        $response = $this->postJson('/api/login/response', [
+            'email' => $this->user->email,
+            'password' => $this->getCurrentPassword(),
+        ]);
+
+        $response
+            ->assertOk()
+            ->assertHasJWT()
+            ->assertHeader('Authorization');
+    }
+
+    /**
+     * Tests that a JWT isn't included in response for login.
+     *
+     * @return void
+     */
+    public function test_invalid_login_json()
+    {
+        $response = $this->postJson('/api/login/response', [
+            'email' => $this->user->email,
+            'password' => $this->faker->password,
+        ]);
+
+        $response
+            ->assertUnauthorized()
+            ->assertJson(['status' => 'error'])
+            ->assertHeaderMissing('Authorization');
+    }
+}
