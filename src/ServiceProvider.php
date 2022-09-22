a9bd9e166bca96d5467cf154168f94ae8bfe3d3d
little-apps <little-apps@users.noreply.github.com>
2022-09-22 07:03:20 +0000

    Fix styling

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace LittleApps\LittleJWT;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response as ResponseFactory;
 
