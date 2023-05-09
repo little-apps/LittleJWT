cbd2bd6d7eb49cbb50385b83ba8b8c244e14201d
Nick H <nick@little-apps.com>
2023-05-08 20:57:14 -0600

    Create HashAlgorithmNotFoundException.php

---
+++ src/Exceptions/HashAlgorithmNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LittleApps\LittleJWT\Exceptions;
+
+use Exception;
+
+/**
+ * Thrown when the hash algorithm for a JWK isn't found.
+ */
+class HashAlgorithmNotFoundException extends Exception
+{
+}
