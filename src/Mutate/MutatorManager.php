7a9987f52b7d6b97c18de0f2f04680965dc39ca0
Nick H <nick@little-apps.com>
2023-05-17 01:09:32 -0600

    Removed unused methods

--- src/Mutate/MutatorManager.php
+++ src/Mutate/MutatorManager.php
@@ -147,36 +147,4 @@ class MutatorManager
     {
         return $mutator->unserialize($value, $key, $args, $jwt);
     }
-
-    /**
-     * Creates a Carbon instance from a value using an (optional) format.
-     *
-     * @param DateTimeInterface|string $value Existing DateTimeInterface or date/time formatted as a string.
-     * @param string|null $format If a string, used as format in $value.
-     * @return Carbon
-     */
-    protected function createCarbonInstance($value, $format = null)
-    {
-        if ($value instanceof DateTimeInterface) {
-            return Carbon::instance($value);
-        }
-
-        return is_string($format) ? Carbon::createFromFormat($format, $value) : Carbon::parse($value);
-    }
-
-    /**
-     * Parses a type mutation.
-     *
-     * @param string $type Type and optional arguments seperated by a :
-     * @return array Array with 2 elements: The mutator type and an array of any optional arguments.
-     */
-    protected function parseMutatorDefinition($type)
-    {
-        $parts = explode(':', $type);
-
-        $mutator = $parts[0];
-        $args = isset($parts[1]) ? explode(',', $parts[1]) : [];
-
-        return [$mutator, $args];
-    }
 }
