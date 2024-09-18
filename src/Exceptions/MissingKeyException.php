359fc537ae1d51059139993246c8eaba0c838476
Nick H <nick@little-apps.com>
2024-09-17 20:52:12 -0600

    Constructor accepts parameters

--- src/Exceptions/MissingKeyException.php
+++ src/Exceptions/MissingKeyException.php
@@ -3,14 +3,15 @@
 namespace LittleApps\LittleJWT\Exceptions;
 
 use Exception;
+use Throwable;
 
 /**
  * Thrown when no secret for the JWK is set.
  */
 class MissingKeyException extends Exception
 {
-    public function __construct()
+    public function __construct(?string $message = null, ?Throwable $previous = null)
     {
-        parent::__construct('The secret for Little JWT is missing.');
+        parent::__construct($message ?? 'The secret for Little JWT is missing.', previous: $previous);
     }
 }
