3ca83d052bed4d2211c6a6875c1a56a9ecfae4e0
Nick H <nick@little-apps.com>
2025-03-01 19:09:08 -0700

    Sets macro for Illuminate\Routing\ResponseFactory directly
    Fixes ResponseFactory being resolved in the container, causing other bindings to be resolved (such as ViewFactory and Redirector), when it may not be used.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,7 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Response as ResponseFactory;
+use Illuminate\Routing\ResponseFactory;
 use LittleApps\LittleJWT\Blacklist\BlacklistManager;
 use LittleApps\LittleJWT\Factories\KeyBuilder;
 use LittleApps\LittleJWT\Factories\LittleJWTBuilder;
